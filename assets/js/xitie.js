e0140f63725ccb144723e444b0702cac2daf9ad6
EthanZeng1992 <ethanzeng1992@gmail.com>
2014-05-30 13:49:46 +0800

    first commit

---
+++ assets/js/xitie.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+
+  //cover page
+  $('#cover').click(function() {
+    $('#cover').slideUp('slow');
+    $('#content').css('display','block');
+    $('.container').css('height','auto');
+  });
+  
+  //background music
+  $("#jquery_jplayer").jPlayer({
+    ready: function () {
+      $(this).jPlayer("setMedia", {
+        m4a: "assets/mp3/come_away_with_me.mp3"
+      }).jPlayer("play");
+    },
+
+
+    supplied: "m4a"
+  });
+
+  var playing = true 
+
+  $("#jquery_jplayer_end").click(function() {
+    if (playing == true) {
+      $(".music").removeClass("music-play").addClass("music-pause")
+      $("#jquery_jplayer").jPlayer("pause");
+      playing = false
+    } else {
+      $(".music").removeClass("music-pause").addClass("music-play")
+      $("#jquery_jplayer").jPlayer("play");
+      playing = true
+    }
+  });
+
+};
